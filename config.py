93f5abb83cc18cbc4130280ec3c862d279862eeb
revanthkumargaming-dotcom <revanthkumargaming@gmail.com>
2026-09-24 20:35:10 +0530

    Update image URL in START_PICS list

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6334669810"))
 # UI URLs - Multiple images that rotate randomly
 # Use DIRECT image URLs (https://i.ibb.co/...) not page URLs (https://ibb.co/...)
 START_PICS = [
-   " https://ibb.co/wN3TXT9V "
+   " https://i.ibb.co/0jjgxKM4/changli-wuthering-waves-4k-wallpaper-uhdpaper-com-437-2-b.jpg"
     # Add more direct image URLs here
 ]
 # CantarellaBots
